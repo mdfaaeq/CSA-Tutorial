01b7a92da43823147449c1497ec30874213bd2e2
Faaeq Fazal <faaeqavl@gmail.com>
2025-04-03 12:21:46 +0530

    Correct main based on the tree structure

--- Tutorial2/src/main/java/com/mycompanny/tutorial2/Tutorial2.java
+++ Tutorial2/src/main/java/com/mycompanny/tutorial2/Tutorial2.java
@@ -8,37 +8,82 @@ package com.mycompanny.tutorial2;
  *
  * @author faaeq
  */
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Tutorial2 {
     public static void main(String[] args) {
-        // Create instances of the validator, data store, and synchronization lock
+        // 1. Create a Scanner object to read user input
+        Scanner scanner = new Scanner(System.in);
+
+        // 2. Create instances of UserValidator and UserDataStore
         UserValidator validator = new UserValidator();
         UserDataStore dataStore = new UserDataStore();
+
+        // 3. Create a List to store Thread objects
+        List<Thread> threadList = new ArrayList<>();
+
+        // 4. Create a lock object for synchronization
         Object lock = new Object();
 
-        // Create UserProcessor threads for two different users
-        UserProcessor processor1 = new UserProcessor(validator, dataStore, lock, "Alice", 25);
-        UserProcessor processor2 = new UserProcessor(validator, dataStore, lock, "Bob", 30);
+        // 5. Initialize a boolean variable to control the input loop
+        boolean addMoreUsers = true;
 
-        // Create threads for each processor
-        Thread thread1 = new Thread(processor1);
-        Thread thread2 = new Thread(processor2);
+        // 2. User Input Loop (while addMoreUsers)
+        while (addMoreUsers) {
+            // 7. Simulate concurrent user input (3 users at a time using for loop)
+            for (int i = 0; i < 3; i++) {
+                // 8. Prompt the user to enter a name
+                System.out.print("Enter user " + (i + 1) + " name: ");
+                String name = scanner.nextLine();
 
-        // Start the threads
-        thread1.start();
-        thread2.start();
+                // 10. Prompt the user to enter an age
+                System.out.print("Enter user " + (i + 1) + " age: ");
+                int age = scanner.nextInt();
 
-        // Wait for threads to finish
-        try {
-            thread1.join();
-            thread2.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+                // 12. Create a UserProcessor object
+                UserProcessor processor = new UserProcessor(validator, dataStore, lock, name, age);
+
+                // 13. Create a Thread object with the UserProcessor object
+                Thread thread = new Thread(processor);
+
+                // 14. Add the thread to the thread list
+                threadList.add(thread);
+
+                // 15. Consume the newline character left by nextInt()
+                scanner.nextLine();
+            }
+
+            // 16. Ask the user if they want to add more users
+            System.out.print("Do you want to add more users? (yes/no): ");
+            String response = scanner.nextLine();
+
+            // 18. Update the loop control variable based on the response
+            if (response.equalsIgnoreCase("no")) {
+                addMoreUsers = false;
+            }
         }
 
-        // Print out all users in the data store
+        // 3. Thread Execution (for Thread t : threadList)
+        for (Thread t : threadList) {
+            try {
+                // 19. Start the thread
+                t.start();
+                // 20. Wait for the thread to finish (join)
+                t.join();
+            } catch (InterruptedException e) {
+                System.out.println("Thread interrupted: " + e.getMessage());
+            }
+        }
+
+        // 4. Displaying Users
         System.out.println("\nAll users stored:");
         for (User user : dataStore.getAllUsers()) {
             System.out.println(user);
         }
+
+        // Close the scanner
+        scanner.close();
     }
 }
